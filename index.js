8ea8f18fd11ce7a74964d5f83029d9808e3beb69
Eric Lathrop <eric@ericlathrop.com>
2014-12-18 16:28:25 -0500

    Simplify ECS so it only manages the code.
    This reduces complexity by removing management of entities and components, and "groups" of systems.
    Since you provide your own entities to the ECS, you can just have two separate ECS instances to create the previous "group" functionality.
    Also, since you must manage entities and groups yourself, you can make your own object pools to reduce allocations.

--- index.js
+++ index.js
@@ -1,59 +1,25 @@
 "use strict";
 
-function ECS() {
-	this.lastId = 0;
-	this.entities = {};
-	this.systems = {};
+function EntityComponentSystem() {
+	this.systems = [];
 }
-ECS.prototype.addEntity = function() {
-	var id = this.lastId;
-	this.entities[id] = { id: id };
-	this.lastId++;
-	return id;
+EntityComponentSystem.prototype.add = function(code) {
+	this.systems.push(code);
 };
-ECS.prototype.getEntity = function(entity) {
-	return this.entities[entity];
-};
-ECS.prototype.getEntities = function() {
-	var ecs = this;
-	return Object.keys(this.entities).map(function(id) {
-		return ecs.entities[id];
+EntityComponentSystem.prototype.addEach = function(code) {
+	this.add(function(entities) {
+		var args = arguments;
+		entities.forEach(function(entity) {
+			args[0] = entity;
+			code.apply(undefined, args);
+		});
 	});
 };
-ECS.prototype.removeEntity = function(entity) {
-	delete this.entities[entity];
-};
-ECS.prototype.addComponent = function(entity, name, component) {
-	this.entities[entity][name] = component;
-};
-ECS.prototype.removeComponent = function(entity, name) {
-	delete this.entities[entity][name];
-};
-ECS.prototype.addSystem = function(group, system) {
-	getSystemGroup(this, group).push(system);
-};
-ECS.prototype.run = function(group) {
-	var args = Array.prototype.slice.call(arguments, 1);
-	var ecs = this;
-	getSystemGroup(ecs, group).forEach(runSystem.bind(this, this, args));
+EntityComponentSystem.prototype.run = function() {
+	var args = arguments;
+	this.systems.forEach(function(system) {
+		system.apply(undefined, args);
+	});
 };
 
-function getSystemGroup(ecs, group) {
-	if (!ecs.systems[group]) {
-		ecs.systems[group] = [];
-	}
-	return ecs.systems[group];
-}
-
-function runSystem(ecs, args, system) {
-	if (system.setup) {
-		system.setup.apply(ecs, args);
-	}
-	if (system.each) {
-		Object.keys(ecs.entities).forEach(function(entity) {
-			system.each.apply(ecs, [ecs.entities[entity]].concat(args));
-		});
-	}
-}
-
-module.exports = ECS;
+module.exports = EntityComponentSystem;
--- index.test.js
+++ index.test.js
@@ -4,133 +4,26 @@ var test = require("tape");
 
 var ECS = require("./index");
 
-test("addEntity returns entityId", function(t) {
+test("run with system and entities calls system with entities", function(t) {
 	t.plan(1);
 
+	var entities = [{}];
 	var ecs = new ECS();
-	var actual = ecs.addEntity();
-
-	t.notEqual(actual, undefined);
-});
-
-test("getEntity with nonexistant entity returns undefined", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var actual = ecs.getEntity("notFound");
-
-	t.equal(actual, undefined);
-});
-
-test("getEntity with existing entity returns object with id", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var id = ecs.addEntity();
-	var actual = ecs.getEntity(id);
-
-	t.deepEqual(actual, { id: id });
-});
-
-test("getEntities with existing entity returns array of entities", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var id = ecs.addEntity();
-	var actual = ecs.getEntities();
-
-	t.deepEqual(actual, [{ id: id }]);
-});
-
-test("removeEntity with existing entity deletes entity", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var entity = ecs.addEntity();
-	ecs.removeEntity(entity);
-	var actual = ecs.getEntity(entity);
-
-	t.equal(actual, undefined);
-});
-
-test("addComponent with nonexistant entity throws", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	t.throws(function() {
-		ecs.addComponent("notFound", "component", {});
-	});
-});
-
-test("addComponent with existing entity adds component", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var id = ecs.addEntity();
-	ecs.addComponent(id, "component", {});
-	t.deepEqual(ecs.getEntity(id), { id: id, component: {} });
-});
-
-test("removeComponent with nonexistant entity throws", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	t.throws(function() {
-		ecs.removeComponent("notFound", "component");
-	});
-});
-
-test("removeComponent with existing entity removes component", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	var id = ecs.addEntity();
-	ecs.addComponent(id, "component", {});
-	ecs.removeComponent(id, "component");
-	t.deepEqual(ecs.getEntity(id), { id: id });
-});
-
-test("run with system with setup calls setup", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	ecs.addEntity();
 	var done = function(arg) {
-		t.deepEqual(arg, "arg");
+		t.deepEqual(arg, entities);
 	};
-	ecs.addSystem("simulation", { setup: done });
-	ecs.run("simulation", "arg");
+	ecs.add(done);
+	ecs.run(entities);
 });
 
-test("run with entity and system calls system with entity", function(t) {
+test("run with each system and entities calls system with each entity", function(t) {
 	t.plan(1);
 
+	var entities = [{}];
 	var ecs = new ECS();
-	var id = ecs.addEntity();
-	var done = function(entity) {
-		t.deepEqual(entity, ecs.getEntity(id));
+	var done = function(arg) {
+		t.deepEqual(arg, entities[0]);
 	};
-	ecs.addSystem("simulation", { each: done });
-	ecs.run("simulation");
-});
-
-test("run with args calls system with args", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	ecs.addEntity();
-	var done = function(entity, arg) { // jshint ignore:line
-		t.equal(arg, 8);
-	};
-	ecs.addSystem("simulation", { each: done });
-	ecs.run("simulation", 8);
-});
-
-test("run with nonexistant group doesn't throw", function(t) {
-	t.plan(1);
-
-	var ecs = new ECS();
-	t.doesNotThrow(function() {
-		ecs.run("simulation");
-	});
+	ecs.addEach(done);
+	ecs.run(entities);
 });
