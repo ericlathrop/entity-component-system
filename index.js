39809ef5bf8c3e8d263a21a419d2207093b79fe6
Eric Lathrop <eric@ericlathrop.com>
2015-04-18 20:50:16 -0400

    Allow user-supplied timer func.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var present = require("present");
-
 function EntityComponentSystem() {
 	this.systems = [];
+	this.now = function() {
+		return 0;
+	}
 }
 EntityComponentSystem.prototype.add = function(code) {
 	this.systems.push(code);
@@ -26,9 +27,9 @@ EntityComponentSystem.prototype.run = function() {
 	var args = arguments;
 	var times = [];
 	for (var i = 0; i < this.systems.length; i++) {
-		var start = present();
+		var start = this.now();
 		this.systems[i].apply(undefined, args);
-		times.push(present() - start);
+		times.push(this.now() - start);
 	}
 	return times;
 };
